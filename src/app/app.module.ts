cfa2347e998d03814c0cbba4152f2a38cc408605
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-09-01 11:45:07 +0530

    1. Imported FormsModule in AppModule. 2. Used template driven forms in Signup page. 3. Added handler for signup page form submit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -31,6 +32,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     FlexLayoutModule,
     MaterialModule,
   ],
