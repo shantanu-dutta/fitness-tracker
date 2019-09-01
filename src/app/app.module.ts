4b99b095e5cf33a8b442eb2c4587ee57a295934e
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-09-01 10:44:24 +0530

    1. Created signup, login components in auth folder. 2. Created training component. 3. Created current-training, new-training, past-trainings in traning component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { TrainingComponent } from './training/training.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    TrainingComponent,
+    CurrentTrainingComponent,
+    NewTrainingComponent,
+    PastTrainingsComponent
   ],
   imports: [
     AppRoutingModule,
