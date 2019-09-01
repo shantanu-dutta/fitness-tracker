623d6e4883fb0c39ca40df9a3ba15bdb0c896e05
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-09-01 12:50:41 +0530

    1. Added MatDatepickerModule, MatNativeDateModule in imports/exports of MaterialModule. 2. Added another mat-form-field with datepicker using material datepicker in signup page.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material';
 
 @NgModule({
   imports: [
     MatButtonModule,
+    MatDatepickerModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatNativeDateModule,
   ],
   exports: [
     MatButtonModule,
+    MatDatepickerModule,
     MatFormFieldModule,
     MatIconModule,
     MatInputModule,
+    MatNativeDateModule,
   ],
 })
 export class MaterialModule { }
