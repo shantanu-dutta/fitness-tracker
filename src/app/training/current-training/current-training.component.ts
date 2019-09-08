586740aeb14e02b1a71c7247d9aee26cb8806ed2
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-09-08 12:40:17 +0530

    1. Added peacock color settings in settings.json. 2. Added functionality to return to new training page when conforming current training stop and resume timer in case denying.

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { MatDialog } from '@angular/material';
 
 import { StopTrainingComponent } from './stop-training.component';
@@ -9,21 +9,37 @@ import { StopTrainingComponent } from './stop-training.component';
   styleUrls: ['./current-training.component.scss']
 })
 export class CurrentTrainingComponent implements OnInit {
+  @Output() trainingExit = new EventEmitter<void>();
   progress = 0;
   timer: number;
 
   constructor(private dialog: MatDialog) { }
 
   ngOnInit() {
+    this.startOrResumeTimer();
+  }
+
+  private startOrResumeTimer() {
     this.timer = window.setInterval(() => {
       this.progress += 5;
-      if (this.progress >= 100) window.clearInterval(this.timer);
+      if (this.progress >= 100) { window.clearInterval(this.timer); }
     }, 1000);
   }
 
   onStop() {
     window.clearInterval(this.timer);
-    this.dialog.open(StopTrainingComponent)
+    const dialogRef = this.dialog.open(StopTrainingComponent, {
+      data: {
+        progress: this.progress
+      }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.trainingExit.emit();
+      } else {
+        this.startOrResumeTimer();
+      }
+    });
   }
 
 }
